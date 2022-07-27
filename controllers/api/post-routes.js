ce2c56630d8c8db7d1364b2abb95170d72de8c6f
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-27 13:54:01 -0400

    added comment and upvote capabilities

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -84,12 +84,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    // make sure the session exists first
+    if (req.session) {
+        //pass session id along eith all destructured properties on req.body
+        Post.upvote({...req.body, user_id: req.session.user_id}, { Vote, Comment, User })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+        });
+    }
 });
 
 router.put('/:id', (req, res) => {
